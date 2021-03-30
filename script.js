94a37a6129d89a2b0bcbb3aaafbad16c383a4890
windowsloth <67842472+windowsloth@users.noreply.github.com>
2021-03-30 18:29:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -286,6 +286,7 @@ function loadTexture(gl, url) {
    
   let testo = 0;
   const image = new Image();
+  image.src = url;
   image.onload = function() {
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
@@ -296,7 +297,6 @@ function loadTexture(gl, url) {
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
     testo = 1;
   }
-  image.src = url;
   
   console.log(testo);
   return texture;
