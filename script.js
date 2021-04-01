ffc38442d19c5bb45f76f6cca103278e2098027d
windowsloth <67842472+windowsloth@users.noreply.github.com>
2021-04-01 10:41:47 -0400

    favor

--- script.js
+++ script.js
@@ -11,7 +11,17 @@ let blue_timeinterval = 1.0;
 let blue_stepsize = 5.0;
 
 let invert = 0.0;
-let imagebool = 0;
+const testimage = new Image();
+let dimensions = new Float32Array([0.0, 0.0]);
+
+$('document').ready(function() {
+  testimage.onload = function() {
+    dimensions[0] = testimage.width;
+    dimensions[1] = testimage.height;
+    main();
+  }
+  testimage.src = './soldiers.jpg'
+});
 
 const vert = `
   attribute vec4 position;
@@ -27,6 +37,8 @@ const frag = `
 
   uniform sampler2D u_image0;
   uniform sampler2D u_image1;
+  uniform vec2 imgdim;
+
   uniform float invert;
   uniform float red_maxtime;
   uniform float red_timeinterval;
@@ -39,9 +51,8 @@ const frag = `
   uniform float blue_stepsize;
 
   varying vec2 texcoord;
-  vec2 imgdim = vec2(2048.0, 1067.0);
   vec2 onepx = vec2(1.0, 1.0) / imgdim;
-  
+
   float scalar = 1.61803398875;
   float hash(vec2 inp, float seed) {
     return fract(tan(distance(inp * scalar, inp) * seed) * inp.x);
@@ -86,7 +97,8 @@ const frag = `
   void main() {
     vec2 coord = gl_FragCoord.xy;
     // float map_result = 0.5 + cos((inversion(invert, texture2D(u_image1, texcoord)) + 1.0) * 3.1415) / 2.0;
-    float map_result = 0.5 + cos((texture2D(u_image1, texcoord).x + 1.0) * 3.1415) / 2.0;
+    // float map_result = 0.5 + cos((texture2D(u_image1, texcoord).x + 1.0) * 3.1415) / 2.0;
+    float map_result = 1.0;
     float redtime = red_maxtime * map_result;
     float greentime = green_maxtime * map_result;
     float bluetime = blue_maxtime * map_result;
@@ -96,16 +108,18 @@ const frag = `
       getColor(coord, 9.2, bluetime, blue_timeinterval, blue_stepsize).z,
       1.0
     );
-    //gl_FragColor = testo;
-    gl_FragColor = texture2D(u_image0, texcoord);
+    gl_FragColor = testo;
+    // gl_FragColor = texture2D(u_image0, texcoord);
   }
 `;
 
 function main() {
 // REFACTORED SHADER CODE BASED ON WEBGL MDN EXAMPLES
 // https://github.com/mdn/webgl-examples
-  
+
   const canvas = document.querySelector('#glcanvas');
+  canvas.width = testimage.width;
+  canvas.height = testimage.height;
   const gl = canvas.getContext('webgl');
 
   // Error handling in case WebGL is not available
@@ -113,7 +127,7 @@ function main() {
     alert('Unable to initialize WebGL. Your browser or machine may not support it.');
     return;
   }
-  
+
   const shaderprogram = initShaderProgram(gl, vert, frag);
   const programinfo = {
     program: shaderprogram,
@@ -121,9 +135,10 @@ function main() {
       position_loc: gl.getAttribLocation(shaderprogram, 'position'),
       texcoord_loc: gl.getAttribLocation(shaderprogram, 'texcoord_attrib'),
     },
-    uniformlocations: {  
+    uniformlocations: {
       image_loc: gl.getUniformLocation(shaderprogram, 'u_image0'),
       height_loc: gl.getUniformLocation(shaderprogram, 'u_image1'),
+      dim_loc: gl.getUniformLocation(shaderprogram, 'imgdim'),
 
       invert_loc: gl.getUniformLocation(shaderprogram, 'invert'),
       red: {
@@ -144,18 +159,82 @@ function main() {
     },
   };
   const buffers = initBuffers(gl);
-//   const textures = [
-//     loadTexture(gl, './soldiers.jpg'),
-//     loadTexture(gl, './h3.jpg')
-//   ];
-  const textures = [];
-  const mainimage = new Image();
-  mainimage.onload = function() {
-    textures.push(loadTexture(gl, mainimage));
-    textures.push(loadTexture(gl, mainimage));
-  };
-  mainimage.src = 'soldiers.jpg';
+
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, testimage);
+
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+  let textures = [
+    texture,
+    loadTexture(gl, './h1.jpg')
+  ];
+
   drawScene(gl, programinfo, buffers, textures);
+
+  $('#red_maxtime').on('input', function() {
+    red_maxtime = $('#red_maxtime').val();
+    drawScene(gl, programinfo, buffers, textures);
+  });
+  $('#red_timeinterval').on('input', function() {
+    red_timeinterval = $('#red_timeinterval').val() / 1000;
+    drawScene(gl, programinfo, buffers, textures);
+  });
+  $('#red_stepsize').on('input', function() {
+    red_stepsize = $('#red_stepsize').val() / 100;
+    drawScene(gl, programinfo, buffers, textures);
+  });
+
+  $('#green_maxtime').on('input', function() {
+    green_maxtime = $('#green_maxtime').val();
+    drawScene(gl, programinfo, buffers, textures);
+  });
+  $('#green_timeinterval').on('input', function() {
+    green_timeinterval = $('#green_timeinterval').val() / 1000;
+    drawScene(gl, programinfo, buffers, textures);
+  });
+  $('#green_stepsize').on('input', function() {
+    green_stepsize = $('#green_stepsize').val() / 100;
+    drawScene(gl, programinfo, buffers, textures);
+  });
+
+  $('#blue_maxtime').on('input', function() {
+    blue_maxtime = $('#blue_maxtime').val();
+    drawScene(gl, programinfo, buffers, textures);
+  });
+  $('#blue_timeinterval').on('input', function() {
+    blue_timeinterval = $('#blue_timeinterval').val() / 1000;
+    drawScene(gl, programinfo, buffers, textures);
+  });
+  $('#blue_stepsize').on('input', function() {
+    blue_stepsize = $('#blue_stepsize').val() / 100;
+    drawScene(gl, programinfo, buffers, textures);
+  });
+  // $('#mainimage').on('input', function() {
+  //   const newsrc = (URL.createObjectURL($('#mainimage').prop('files')[0]));
+  //   testimage.src = newsrc;
+  //   canvas.width = testimage.width;
+  //   canvas.height = testimage.height;
+  //   let newtexture = gl.createTexture();
+  //   gl.bindTexture(gl.TEXTURE_2D, newtexture);
+  //   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, testimage);
+  //
+  //   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  //   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  //   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  //   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+  //
+  //   textures[0] = newtexture;
+  //   drawScene(gl, programinfo, buffers, textures);
+  // });
+  $('#save').on('click', function() {
+    $('#content').append('<img src="' + result + '">');
+  });
+  // drawScene(gl, programinfo, buffers, textures);
 }
 function initShaderProgram(gl, vertsource, fragsource) {
   // Load in the vertex and fragment shaders from our global variables
@@ -166,13 +245,13 @@ function initShaderProgram(gl, vertsource, fragsource) {
   gl.attachShader(shaderprogram, vertexshader);
   gl.attachShader(shaderprogram, fragmentshader);
   gl.linkProgram(shaderprogram);
-  
+
   // Handle errors from the program
   if (!gl.getProgramParameter(shaderprogram, gl.LINK_STATUS)) {
     console.log('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderprogram));
     return null;
   }
-  
+
   return shaderprogram;
 }
 
@@ -180,14 +259,14 @@ function loadShader(gl, type, source) {
   const shader = gl.createShader(type);
   gl.shaderSource(shader, source);
   gl.compileShader(shader);
-  
+
   // Handle errors in compiling shader
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
     alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
     gl.deleteShader(shader);
     return null;
   }
-  
+
   return shader;
 }
 
@@ -205,7 +284,7 @@ function initBuffers(gl) {
   const posbuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, posbuffer);
   gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-   
+
   const texcoords = new Float32Array([
     0.0, 0.0,
     1.0, 0.0,
@@ -218,11 +297,15 @@ function initBuffers(gl) {
   const texcoordbuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, texcoordbuffer);
   gl.bufferData(gl.ARRAY_BUFFER, texcoords, gl.STATIC_DRAW);
-  
+
   const invertbuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, invertbuffer);
   gl.bufferData(gl.ARRAY_BUFFER, invert, gl.STATIC_DRAW);
-  
+
+  const dimbuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, dimbuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, dimensions, gl.STATIC_DRAW);
+
   //======= R E D == B U F F E R S ===============================================
   let red_maxtimebuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, red_maxtimebuffer);
@@ -261,10 +344,11 @@ function initBuffers(gl) {
   let blue_stepsizebuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, blue_stepsizebuffer);
   gl.bufferData(gl.ARRAY_BUFFER, blue_stepsize, gl.STATIC_DRAW);
-  
+
   return {
     position: posbuffer,
     texcoord: texcoordbuffer,
+    imgdim: dimbuffer,
     invert: invertbuffer,
     red: {
       maxtime: red_maxtimebuffer,
@@ -284,34 +368,18 @@ function initBuffers(gl) {
   };
 }
 
-function loadTexture(gl, img) {
+function loadTexture(gl, url) {
   // Load in a white pixel as a placeholder while the image(s) load
   const pixel = new Uint8Array([255, 255, 255, 255]);
   const texture = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, texture);
-//   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, pixel);
-//   
-//   const image = new Image();
-//   image.onload = function() {
-//     gl.bindTexture(gl.TEXTURE_2D, texture);
-//     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-//     
-//     if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
-//        // Yes, it's a power of 2. Generate mips.
-//        gl.generateMipmap(gl.TEXTURE_2D);
-//     } else {
-//        // No, it's not a power of 2. Turn of mips and set
-//        // wrapping to clamp to edge
-//        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-//        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-//        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-//     }
-//     console.log('image ' + url + ' is loaded, finally');
-//     imagebool++;
-//   }
-//   image.src = url;
-  
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, pixel);
+
+  const image = new Image();
+  image.onload = function() {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
     if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
        // Yes, it's a power of 2. Generate mips.
        gl.generateMipmap(gl.TEXTURE_2D);
@@ -322,6 +390,10 @@ function loadTexture(gl, img) {
        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     }
+    console.log('image ' + url + ' is loaded, finally');
+  }
+  image.src = url;
+
   return texture;
 }
 
@@ -334,7 +406,7 @@ function drawScene(gl, programinfo, buffers, textures) {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clearDepth(1.0); // Is this line necessary if I'm doing 2D? No depth testing required afaik
   gl.clear(gl.COLOR_BUFFER_BIT);
-  
+
   // Tell the program where/how to get the position data for the vertex shader
   { // Position
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
@@ -346,9 +418,9 @@ function drawScene(gl, programinfo, buffers, textures) {
     gl.vertexAttribPointer(programinfo.attriblocations.texcoord_loc, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(programinfo.attriblocations.texcoord_loc);
   }
-  
+
   gl.useProgram(programinfo.program);
-  
+
   { // Main Image
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, textures[0]);
@@ -359,11 +431,15 @@ function drawScene(gl, programinfo, buffers, textures) {
     gl.bindTexture(gl.TEXTURE_2D, textures[1]);
     gl.uniform1i(programinfo.uniformlocations.height_loc, 1);
   }
+  { // Image Dimensions
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.imgdim);
+    gl.uniform2fv(programinfo.uniformlocations.dim_loc, dimensions);
+  }
   { // Height Map Inversion
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.invert);
     gl.uniform1f(programinfo.uniformlocations.invert_loc, invert);
   }
-  
+
   // Might make a seperate function to hold all of the individual uniform bindings for each channel
   // RED CHANNEL
   { // Max time
@@ -372,27 +448,27 @@ function drawScene(gl, programinfo, buffers, textures) {
   }
   { // Time interval
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.red.timeinterval);
-    gl.uniform1f(programinfo.uniformlocations.red.maxtimeloc, red_timeinterval);
+    gl.uniform1f(programinfo.uniformlocations.red.timeintervalloc, red_timeinterval);
   }
   { // Step size
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.red.stepsize);
-    gl.uniform1f(programinfo.uniformlocations.red.maxtimeloc, red_stepsize);
+    gl.uniform1f(programinfo.uniformlocations.red.stepsizeloc, red_stepsize);
   }
-  
+
     // GREEN CHANNEL
   { // Max time
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.green.maxtime);
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.green.maxtimebuffer);
     gl.uniform1f(programinfo.uniformlocations.green.maxtimeloc, green_maxtime);
   }
   { // Time interval
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.green.timeinterval);
-    gl.uniform1f(programinfo.uniformlocations.green.maxtimeloc, green_timeinterval);
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.green.maxtimebuffer);
+    gl.uniform1f(programinfo.uniformlocations.green.timeintervalloc, green_timeinterval);
   }
   { // Step size
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.green.stepsize);
-    gl.uniform1f(programinfo.uniformlocations.green.maxtimeloc, green_stepsize);
+    gl.uniform1f(programinfo.uniformlocations.green.stepsizeloc, green_stepsize);
   }
-  
+
     // BLUE CHANNEL
   { // Max time
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.blue.maxtime);
@@ -400,12 +476,13 @@ function drawScene(gl, programinfo, buffers, textures) {
   }
   { // Time interval
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.blue.timeinterval);
-    gl.uniform1f(programinfo.uniformlocations.blue.maxtimeloc, blue_timeinterval);
+    gl.uniform1f(programinfo.uniformlocations.blue.timeintervalloc, blue_timeinterval);
   }
   { // Step size
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.blue.stepsize);
-    gl.uniform1f(programinfo.uniformlocations.blue.maxtimeloc, blue_stepsize);
+    gl.uniform1f(programinfo.uniformlocations.blue.stepsizeloc, blue_stepsize);
   }
-  
+
   gl.drawArrays(gl.TRIANGLES, 0, 6);
+  result = gl.canvas.toDataURL();
 }
