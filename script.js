3403fd7f2ebbaa419129c6aacd519813b36b5ea6
windowsloth <67842472+windowsloth@users.noreply.github.com>
2021-04-01 12:59:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -173,7 +173,8 @@ async function main() {
 //     texture,
 //     loadTexture(gl, './h1.jpg')
 //   ];
-  console.log('here is a weird thing: ' + await loadImage('soldiers.jpg'));
+  let testomania = await loadImage('soldiers.jpg);
+                                   console.log('here is a weird thing: ' + testomania);
   const imagetexture = p_loadTexture(gl, await loadImage('soldiers.jpg'));
   const heightmaptexture = p_loadTexture(gl, await loadImage('h1.jpg'));
   const textures = [imagetexture, heightmaptexture];
